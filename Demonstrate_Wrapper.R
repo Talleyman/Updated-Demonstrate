9c7b48fa9e1f60bda4123c6853445320e0242263
Talleyman <Talleyman@users.noreply.github.com>
2015-03-06 13:03:26 -0500

    Update Demonstrate_Wrapper.R

--- Demonstrate_Wrapper.R
+++ Demonstrate_Wrapper.R
@@ -1,7 +1,10 @@
-#Test edit
+#Wrapper script for Demonstrate package for use on Stampede
+#Written by: Marco Martinez and Stephen Talley
 args <- commandArgs(TRUE)
+#Load up command line arguments
 dir <- as.character(args[1])
-
+AUC.title<-as.character(args[2])
+MAE.title<-as.character(args[3])
 
 source("Demonstrate_Stampede_Edition.R")
 source("lineplotCI.R")
